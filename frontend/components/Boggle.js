a51f42c9c1e53b095b880db759163dd8bf32bb32
captcontrol <pridham.tom@gmail.com>
2016-10-05 13:59:25 -0600

    created board and tile components

--- frontend/components/Boggle.js
+++ frontend/components/Boggle.js
@@ -1,11 +1,58 @@
 `use strict`;
 
 import React from 'react';
+import BoggleTile from './Boggle/BoggleTile';
 
-class Boggle extends React.Component{
-     render() {
+class Boggle extends React.Component {
+
+
+    componentWillMount() {
+        this.generateBoard();
+        this.getWordList();
+    }
+
+    componentDidMount() {
+        //add listener for enter key to check words
+        document.getElementById('input')
+            .addEventListener('keydown', event => {
+                if (event.keyCode === 13) {
+                    this.checkWord()
+                }
+            })
+    }
+
+    generateBoard() {
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        let tiles = [];
+
+        for (let i = 0; i < 16; i++) {
+            tiles.push(<BoggleTile key={i} letter={chars[Math.floor(Math.random() * chars.length)]}/>)
+        }
+        this.setState({tiles: tiles});
+    }
+
+    getWordList() {
+
+        const checkAdjacent = ()=>{};
+
+        this.state.tiles.forEach(tile=>{
+
+        })
+
+    }
+
+    checkWord() {
+
+    }
+
+    render() {
         return (
-            <div>some boggle stuff</div>
+            <div>
+                <div className="board">
+                    {this.state.tiles}
+                </div>
+                <input id='input' type="text"/>
+            </div>
         )
     }
 }
--- frontend/components/Boggle/BoggleTile.js
+++ frontend/components/Boggle/BoggleTile.js
@@ -1,1 +1,16 @@
 `use strict`;
+
+import React from 'react';
+
+class BoggleTile extends React.Component{
+
+    render() {
+        return (
+            <div className="col-xs-3 tile" >
+                <h2>{this.props.letter}</h2>
+            </div>
+        )
+    }
+}
+
+export default BoggleTile;
