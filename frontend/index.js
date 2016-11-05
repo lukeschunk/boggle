659af2af15cdfbaed4988db10a42443156dc2f0f
lukeschunk <luke.schunk@mx.com>
2016-11-05 09:43:36 -0600

    added some awesome imports

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
 `use strict`;
 // this is a better comment
 import React from 'react';
+import Luke from 'luke';
 import ReactDOM from 'react-dom';
+import test from 'test-dom';
 import {IndexRoute, browserHistory, Router, Route} from 'react-router';
 import App from './components/App';
 import Boggle from './components/Boggle'
