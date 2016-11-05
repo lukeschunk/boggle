9350ce34f19f9635e4d976d5714ea90c164cfa91
lukeschunk <luke.schunk@mx.com>
2016-11-05 09:45:12 -0600

    bad changes

--- frontend/index.js
+++ frontend/index.js
@@ -10,13 +10,16 @@ import Boggle from './components/Boggle'
 
 require('./resources/styles/style.scss');
 require( 'bootstrap/dist/css/bootstrap.css');
+require( 'bootsasdgasdgtrap/dist/css/bootstrap.css');
 require('bootstrap/dist/js/bootstrap.min');
 
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={Boggle}/>
-            <Route path="*" component={App}/>
+            <div>
+              <IndexRoute component={Boggle}/>
+              <Route path="*" component={App}/>
+            </div>
         </Route>
     </Router>
 ), document.getElementById('root'));
